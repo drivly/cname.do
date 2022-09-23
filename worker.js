a6473be3efd2d8f3b29f1af3b9f36ead33279f12
Nathan Clevenger <nateclev@gmail.com>
2022-09-23 06:24:00 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -19,15 +19,7 @@ export default {
   fetch: async (req, env) => {
     const { user, origin, requestId, method, body, time, pathname, pathSegments, pathOptions, url, query } = await env.CTX.fetch(req).then(res => res.json())
     
-    const matcher = /(?<method>.+)\((?<args>.+)\)/g
-    
-    const exec = pathSegments.map(segment => segment.match(matcher))
-    
-    const methods = Object.keys(_).reduce((acc, method) => {
-      acc[method] = `https://lodash.do/${method}/:args${pathname}`
-      return acc
-    }, {})
 
-    return new Response(JSON.stringify({ api, url, pathSegments, pathOptions, exec, methods, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
+    return new Response(JSON.stringify({ api, url, pathSegments, pathOptions, methods, user }, null, 2), { headers: { 'content-type': 'application/json; charset=utf-8' }})
   },
 }
